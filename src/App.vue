d149b2e4f24bbe122e1f3b5fe3f947f096f8ec21
Ozmeks <omehryakov@gmail.com>
2026-06-15 12:19:44 +0200

    Add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,7 @@
 <template>
-  <div class="app">
-    <nav class="nav">
-      <ProfileAvatar :name="name" />
-    </nav>
-    <div class="card-grid">
-      <MeditationCard
-        v-for="meditation in store.meditations"
-        :key="meditation.id"
-        :meditation="meditation"
-      />
-    </div>
-  </div>
+  <RouterView />
 </template>
 
 <script setup lang="ts">
-import { useMeditationsStore } from '@/stores/meditations.store';
-import { onMounted } from 'vue';
-import MeditationCard from './components/MeditationCard.vue';
-import ProfileAvatar from './components/ProfileAvatar.vue';
-
-const store = useMeditationsStore();
-
-onMounted(() => {
-  store.fetchMeditations();
-});
-
-const name = 'Наталья';
+import { RouterView } from 'vue-router';
 </script>
-
-<style scoped>
-.card-grid {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(340px, 1fr));
-  gap: 20px;
-  width: 100%;
-}
-.app {
-  display: flex;
-  margin-top: 150px;
-}
-.nav {
-  min-width: 500px;
-}
-</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <ProfileAvatar :name="name" />
+    <div class="card-grid">
+      <MeditationCard
+        v-for="meditation in store.meditations"
+        :key="meditation.id"
+        :meditation="meditation"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useMeditationsStore } from '@/stores/meditations.store';
+import { onMounted } from 'vue';
+import MeditationCard from '@/components/MeditationCard.vue';
+import ProfileAvatar from '@/components/ProfileAvatar.vue';
+
+const store = useMeditationsStore();
+
+onMounted(() => {
+  store.fetchMeditations();
+});
+
+const name = 'Наталья';
+</script>
+
+<style scoped>
+.card-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(340px, 1fr));
+  gap: 20px;
+  width: 100%;
+}
+.home {
+  display: flex;
+  margin-top: 150px;
+}
+</style>
